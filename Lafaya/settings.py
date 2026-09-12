d43bbec63a1ed1b08e676b7406ee34e546f3786c
Sunhaloo <azmaan151003@gmail.com>
2026-09-12 13:39:58 +0400

    U: Added the global `static` directory at the root of the repository + Configured `settings.py` file

--- Lafaya/settings.py
+++ Lafaya/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_URL = "static/"
 
 
