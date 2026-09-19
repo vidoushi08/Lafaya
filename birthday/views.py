9bc0f2ff0a4ad68623f21be3406cf455542d8bdd
Nidhi dhunputh <nidhi.dhunputh@umail.uom.ac.mu>
2026-09-19 21:56:37 +0400

    set up for birthday theme

--- birthday/views.py
+++ birthday/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Birthday")
+    return render(request, "birthday/birthday.html")
