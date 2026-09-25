95efa331bb40f6ee63570bb983662b0c161f54b8
vidoushi08 <vidoushisoobug@gmail.com>
2026-09-25 09:44:04 +0400

    A + U: Initial draft for authentication django app

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Authentication")
+    return render(request, "authentication/authentication.html")
